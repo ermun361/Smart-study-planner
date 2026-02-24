59891163f5633350c75581e5939a781dad1ccaa4
Eric Munyi <munyieric7@gmail.com>
2026-02-24 16:34:21 +0300

    feat: protect dashboard routes in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,26 @@
 import React, { useState } from 'react'; // 1. Added useState
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+
+import { useAuthStore } from './store/useAuthStore';
+
+// LAYOUT & PAGES
 import MainLayout from './layout/MainLayout';
 import Dashboard from './pages/Dashboard';
 import SubjectsPage from './pages/Subjects'; 
 import AddSubjectModal from './components/AddSubjectModal';
 import ProgressPage from './pages/ProgressPage';
 import LandingPage from './pages/LandingPage';
+import LoginPage from './pages/LoginPage';
 
 // Placeholders for pages NOT yet built 
 const SettingsPage = () => <div className="p-8 text-2xl font-bold italic text-gray-400">Preferences (Week 3)</div>;
 
 function App() {
-  // 3. Define the Modal state here so it can be opened from any page
+  const isAuthenticated = useAuthStore((state) => state.isAuthenticated);
+
+
+
+  // The Modal state 
   const [isModalOpen, setIsModalOpen] = useState(false);
 
   return (
@@ -19,9 +28,10 @@ function App() {
       <Routes>
         {/* --- 1. PUBLIC ROUTE  --- */}
         <Route path="/" element={<LandingPage />} />
+        <Route path="/login" element={<LoginPage />} />
 
         {/* --- 2. PRIVATE APP ROUTES */}
-        <Route element={<MainLayout />}>
+        <Route element={isAuthenticated ? <MainLayout /> : <Navigate to="/login" replace />}>
           {/* We remove the Navigate from "/" so it doesn't skip the Landing Page */}
           
           <Route 
---
+++ src/pages/LoginPage.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const LoginPage = () => {
+    return (
+        <div className="min-h-screen bg-[#cbd5e1] flex items-center justify-center">
+            <h1 className="text-2xl font-bold">Login Page Initialized</h1>
+        </div>
+    )
+}
+export default LoginPage;

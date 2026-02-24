2d565638d53efc74f7141035a771d2902045513a
Eric Munyi <munyieric7@gmail.com>
2026-02-24 12:12:47 +0300

    feat: add hero section

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import { Book } from 'lucide-react';
+import { useNavigate } from 'react-router-dom';
 
 
 const LandingPage = () => {
+    const navigate = useNavigate();
     return (
         <div className="min-h-screen bg-[#cbd5e1] text-slate-900">
             <nav className="flex items-center justify-between px-8 py-6 max-w-7xl mx-auto">
@@ -14,8 +16,20 @@ const LandingPage = () => {
                 </div>
                 <button className="bg-slate-400/40 px-8 py-2 rounded-xl font-bold">Login</button>
             </nav>
-            <main className="max-w-7xl mx-auto px-8 pt-20">
-                <p>Content goes here...</p>
+            <main className="max-w-7xl mx-auto px-8 pt-12 grid lg:grid-cols-2 items-center">
+                <div className="space-y-8">
+                    <h1 className="text-6xl md:text-8xl font-bold text-slate-950 leading-[0.9] tracking-tighter">
+                        Smart Study <br /> Planner
+                    </h1>
+                    <p className="text-xl md:text-2xl text-slate-700 font-medium">
+                        Plan your studies intelligently and stay on track.
+                    </p>
+                    <button 
+                    onClick={() => navigate('/dashboard')}
+                    className="bg-[#5e5ce6] text-white text-2xl font-bold px-12 py-5 rounded-3xl shadow-2xl">
+                        Get Started
+                    </button>
+                </div>
             </main>
             
         </div>
